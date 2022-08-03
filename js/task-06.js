0157bfc461e4f2dadf5602216880a8508d8e1593
timoxa2t <alex2t@ukr.net>
2022-08-03 17:58:39 +0300

    task 6 correction

--- js/task-06.js
+++ js/task-06.js
@@ -2,9 +2,9 @@ const inputEl = document.querySelector("#validation-input");
 inputEl.addEventListener("blur", () => {
   if (inputEl.value.length < inputEl.getAttribute("data-length")) {
     inputEl.classList.remove("valid");
-    inputEl.classList.toggle("invalid");
+    inputEl.classList.add("invalid");
   } else {
     inputEl.classList.remove("invalid");
-    inputEl.classList.toggle("valid");
+    inputEl.classList.add("valid");
   }
 });
